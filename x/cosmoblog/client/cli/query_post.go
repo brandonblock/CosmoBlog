e9a62004da6f34a4b3e0551aa687fa7d4223e08c
Brandon Block <brandon.block@pm.me>
2021-10-15 11:40:59 -0500

    command stubs

---
+++ x/cosmoblog/client/cli/query_post.go
@@ -0,0 +1,18 @@
+package cli
+
+import (
+	// "context"
+
+	"github.com/spf13/cobra"
+	// "github.com/cosmos/cosmos-sdk/client"
+	// "github.com/cosmos/cosmos-sdk/client/flags"
+	// "github.com/example/blog/x/blog/types"
+)
+
+func CmdListPost() *cobra.Command {
+	return nil
+}
+
+func CmdShowPost() *cobra.Command {
+	return nil
+}
