82621bc5772e65d3697342d728e3d454fe656c83
Brandon Block <brandon.block@pm.me>
2021-10-14 20:23:58 -0500

    start create cmd post

--- x/cosmoblog/client/cli/tx.go
+++ x/cosmoblog/client/cli/tx.go
@@ -37,5 +37,20 @@ func GetTxCmd() *cobra.Command {
 }
 
 func CmdCreatePost() *cobra.Command {
-	return nil
+	cmd := &cobra.Command{
+		Use:   "create-post [title] [body]",
+		Short: "Creates a new post",
+		Args:  cobra.ExactArgs(2),
+		// RunE: func(cmd *cobra.Command, args []string) error {
+		// 	argsTitle := string(args[0])
+		// 	argsBody := string(args[1])
+
+		// 	clientCtx, err := client.GetClientTxContext(cmd)
+		// 	if err != nil {
+		// 		log.Err("unable to get client TX Context, err:", err)
+		// 	}
+
+		// },
+	}
+	return cmd
 }
